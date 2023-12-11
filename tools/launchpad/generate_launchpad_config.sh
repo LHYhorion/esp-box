aea0f6a19dace7f874653b454ffe7784b5017fef
Lv Haiyu <lvhaiyu@espressif.com>
2023-12-11 19:45:06 +0800

    test script 21.

--- tools/launchpad/generate_launchpad_config.sh
+++ tools/launchpad/generate_launchpad_config.sh
@@ -26,7 +26,7 @@ OUT_FILE=launchpad.toml
 
 cat <<EOF >> $OUT_FILE
 esp_toml_version = 1.0
-firmware_images_url = "https://github.com/LHYhorion/esp-box/"
+firmware_images_url = "https://lhyhorion.github.io/esp-box/"
 
 EOF
 
