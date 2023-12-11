74908473ea2a3db38b7e305c1099b776d15e9eb0
Lv Haiyu <lvhaiyu@espressif.com>
2023-12-11 16:37:36 +0800

    test script 14.

--- tools/launchpad/rename.sh
+++ tools/launchpad/rename.sh
@@ -110,5 +110,93 @@ for file in *; do
     esac
 
     mv "$file" "$newfile"
+  elif [[ $file == *image_display_sdkconfig.ci* ]]; then
+    version=$(grep -E "#define MATTER_SWITCH_VERSION_(MAJOR|MINOR|PATCH)" $@/examples/image_display/main/image_display.h | awk '{print $3}')
+    version="${version//[$'\t\r\n ']/}"
+
+    version_with_dots=$(echo "$version" | sed 's/\(.\)/\1_/g' | sed 's/\_$//')
+
+    case "$file" in
+      *_sdkconfig.ci.box-3*)
+        newfile=$(echo "$file" | sed "s/_sdkconfig.ci.box-3/-ESP-BOX-3-$version_with_dots/g")
+        ;;
+      *_sdkconfig.ci.box-lite*)
+        newfile=$(echo "$file" | sed "s/_sdkconfig.ci.box-lite/-ESP-BOX-Lite-$version_with_dots/g")
+        ;;
+      *_sdkconfig.ci.box*)
+        newfile=$(echo "$file" | sed "s/_sdkconfig.ci.box/-ESP-BOX-$version_with_dots/g")
+        ;;
+      *)
+        newfile="$file"
+        ;;
+    esac
+
+    mv "$file" "$newfile"
+  elif [[ $file == *lv_demos_sdkconfig.ci* ]]; then
+    version=$(grep -E "#define MATTER_SWITCH_VERSION_(MAJOR|MINOR|PATCH)" $@/examples/lv_demos/main/lv_demos.h | awk '{print $3}')
+    version="${version//[$'\t\r\n ']/}"
+
+    version_with_dots=$(echo "$version" | sed 's/\(.\)/\1_/g' | sed 's/\_$//')
+
+    case "$file" in
+      *_sdkconfig.ci.box-3*)
+        newfile=$(echo "$file" | sed "s/_sdkconfig.ci.box-3/-ESP-BOX-3-$version_with_dots/g")
+        ;;
+      *_sdkconfig.ci.box-lite*)
+        newfile=$(echo "$file" | sed "s/_sdkconfig.ci.box-lite/-ESP-BOX-Lite-$version_with_dots/g")
+        ;;
+      *_sdkconfig.ci.box*)
+        newfile=$(echo "$file" | sed "s/_sdkconfig.ci.box/-ESP-BOX-$version_with_dots/g")
+        ;;
+      *)
+        newfile="$file"
+        ;;
+    esac
+
+    mv "$file" "$newfile"
+  elif [[ $file == *mp3_demo_sdkconfig.ci* ]]; then
+    version=$(grep -E "#define MATTER_SWITCH_VERSION_(MAJOR|MINOR|PATCH)" $@/examples/mp3_demo/main/mp3_demo.h | awk '{print $3}')
+    version="${version//[$'\t\r\n ']/}"
+
+    version_with_dots=$(echo "$version" | sed 's/\(.\)/\1_/g' | sed 's/\_$//')
+
+    case "$file" in
+      *_sdkconfig.ci.box-3*)
+        newfile=$(echo "$file" | sed "s/_sdkconfig.ci.box-3/-ESP-BOX-3-$version_with_dots/g")
+        ;;
+      *_sdkconfig.ci.box-lite*)
+        newfile=$(echo "$file" | sed "s/_sdkconfig.ci.box-lite/-ESP-BOX-Lite-$version_with_dots/g")
+        ;;
+      *_sdkconfig.ci.box*)
+        newfile=$(echo "$file" | sed "s/_sdkconfig.ci.box/-ESP-BOX-$version_with_dots/g")
+        ;;
+      *)
+        newfile="$file"
+        ;;
+    esac
+
+    mv "$file" "$newfile"
+  elif [[ $file == *watering_demo_sdkconfig.ci* ]]; then
+    version=$(grep -E "#define MATTER_SWITCH_VERSION_(MAJOR|MINOR|PATCH)" $@/examples/watering_demo/main/main.h | awk '{print $3}')
+    version="${version//[$'\t\r\n ']/}"
+
+    version_with_dots=$(echo "$version" | sed 's/\(.\)/\1_/g' | sed 's/\_$//')
+
+    case "$file" in
+      *_sdkconfig.ci.box-3*)
+        newfile=$(echo "$file" | sed "s/_sdkconfig.ci.box-3/-ESP-BOX-3-$version_with_dots/g")
+        ;;
+      *_sdkconfig.ci.box-lite*)
+        newfile=$(echo "$file" | sed "s/_sdkconfig.ci.box-lite/-ESP-BOX-Lite-$version_with_dots/g")
+        ;;
+      *_sdkconfig.ci.box*)
+        newfile=$(echo "$file" | sed "s/_sdkconfig.ci.box/-ESP-BOX-$version_with_dots/g")
+        ;;
+      *)
+        newfile="$file"
+        ;;
+    esac
+
+    mv "$file" "$newfile"
   fi
 done
