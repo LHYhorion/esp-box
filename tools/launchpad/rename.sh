c4e4ed80eafe7e7ce0a369b0093b6be3fc15c146
Lv Haiyu <lvhaiyu@espressif.com>
2023-12-11 14:53:17 +0800

    test script 12.

--- tools/launchpad/rename.sh
+++ tools/launchpad/rename.sh
@@ -1,148 +1,57 @@
 #!/bin/bash
 for file in *; do
-  if [[ $file == *chatgpt_demo_sdkconfig.ci.box-3* ]]; then
+  if [[ $file == *chatgpt_demo_sdkconfig.ci* ]]; then
     version=$(grep -E "#define CHATGPT_DEMO_VERSION_(MAJOR|MINOR|PATCH)" $@/examples/chatgpt_demo/main/main.h | awk '{print $3}')
     version="${version//[$'\t\r\n ']/}"
 
     version_with_dots=$(echo "$version" | sed 's/\(.\)/\1_/g' | sed 's/\_$//')
 
     newfile=$(echo "$file" | sed "s/_sdkconfig.ci.box-3/-ESP-BOX-3-$version_with_dots/g")
-
-    mv "$file" "$newfile"
-
-  elif [[ $file == *chatgpt_demo_sdkconfig.ci.box-lite* ]]; then
-    version=$(grep -E "#define CHATGPT_DEMO_VERSION_(MAJOR|MINOR|PATCH)" $@/examples/chatgpt_demo/main/main.h | awk '{print $3}')
-    version="${version//[$'\t\r\n ']/}"
-
-    version_with_dots=$(echo "$version" | sed 's/\(.\)/\1_/g' | sed 's/\_$//')
-
     newfile=$(echo "$file" | sed "s/_sdkconfig.ci.box-lite/-ESP-BOX-Lite-$version_with_dots/g")
-
-    mv "$file" "$newfile"
-
-  elif [[ $file == *chatgpt_demo_sdkconfig.ci.box* ]]; then
-    version=$(grep -E "#define CHATGPT_DEMO_VERSION_(MAJOR|MINOR|PATCH)" $@/examples/chatgpt_demo/main/main.h | awk '{print $3}')
-    version="${version//[$'\t\r\n ']/}"
-
-    version_with_dots=$(echo "$version" | sed 's/\(.\)/\1_/g' | sed 's/\_$//')
-
     newfile=$(echo "$file" | sed "s/_sdkconfig.ci.box/-ESP-BOX-$version_with_dots/g")
 
     mv "$file" "$newfile"
-
-  elif [[ $file == *factory_demo_sdkconfig.ci.box-3* ]]; then
+  elif [[ $file == *factory_demo_sdkconfig.ci* ]]; then
     version=$(grep -E "#define BOX_DEMO_VERSION_(MAJOR|MINOR|PATCH)" $@/examples/factory_demo/main/main.h | awk '{print $3}')
     version="${version//[$'\t\r\n ']/}"
 
     version_with_dots=$(echo "$version" | sed 's/\(.\)/\1_/g' | sed 's/\_$//')
 
     newfile=$(echo "$file" | sed "s/_sdkconfig.ci.box-3/-ESP-BOX-3-$version_with_dots/g")
-
-    mv "$file" "$newfile"
-
-  elif [[ $file == *factory_demo_sdkconfig.ci.box-lite* ]]; then
-    version=$(grep -E "#define BOX_DEMO_VERSION_(MAJOR|MINOR|PATCH)" $@/examples/factory_demo/main/main.h | awk '{print $3}')
-    version="${version//[$'\t\r\n ']/}"
-
-    version_with_dots=$(echo "$version" | sed 's/\(.\)/\1_/g' | sed 's/\_$//')
-
     newfile=$(echo "$file" | sed "s/_sdkconfig.ci.box-lite/-ESP-BOX-Lite-$version_with_dots/g")
-
-    mv "$file" "$newfile"
-
-  elif [[ $file == *factory_demo_sdkconfig.ci.box* ]]; then
-    version=$(grep -E "#define BOX_DEMO_VERSION_(MAJOR|MINOR|PATCH)" $@/examples/factory_demo/main/main.h | awk '{print $3}')
-    version="${version//[$'\t\r\n ']/}"
-
-    version_with_dots=$(echo "$version" | sed 's/\(.\)/\1_/g' | sed 's/\_$//')
-
     newfile=$(echo "$file" | sed "s/_sdkconfig.ci.box/-ESP-BOX-$version_with_dots/g")
 
     mv "$file" "$newfile"
-  elif [[ $file == *usb_headset_sdkconfig.ci.box-3* ]]; then
+  elif [[ $file == *usb_headset_sdkconfig.ci* ]]; then
     version=$(grep -E "#define USB_HEADSET_VERSION_(MAJOR|MINOR|PATCH)" $@/examples/usb_headset/main/main.h | awk '{print $3}')
     version="${version//[$'\t\r\n ']/}"
 
     version_with_dots=$(echo "$version" | sed 's/\(.\)/\1_/g' | sed 's/\_$//')
 
     newfile=$(echo "$file" | sed "s/_sdkconfig.ci.box-3/-ESP-BOX-3-$version_with_dots/g")
-
-    mv "$file" "$newfile"
-
-  elif [[ $file == *usb_headset_sdkconfig.ci.box-lite* ]]; then
-    version=$(grep -E "#define USB_HEADSET_VERSION_(MAJOR|MINOR|PATCH)" $@/examples/usb_headset/main/main.h | awk '{print $3}')
-    version="${version//[$'\t\r\n ']/}"
-
-    version_with_dots=$(echo "$version" | sed 's/\(.\)/\1_/g' | sed 's/\_$//')
-
     newfile=$(echo "$file" | sed "s/_sdkconfig.ci.box-lite/-ESP-BOX-Lite-$version_with_dots/g")
-
-    mv "$file" "$newfile"
-
-  elif [[ $file == *usb_headset_sdkconfig.ci.box* ]]; then
-    version=$(grep -E "#define USB_HEADSET_VERSION_(MAJOR|MINOR|PATCH)" $@/examples/usb_headset/main/main.h | awk '{print $3}')
-    version="${version//[$'\t\r\n ']/}"
-
-    version_with_dots=$(echo "$version" | sed 's/\(.\)/\1_/g' | sed 's/\_$//')
-
     newfile=$(echo "$file" | sed "s/_sdkconfig.ci.box/-ESP-BOX-$version_with_dots/g")
 
     mv "$file" "$newfile"
-  elif [[ $file == *usb_camera_sdkconfig.ci.box-3* ]]; then
+  elif [[ $file == *usb_camera_sdkconfig.ci* ]]; then
     version=$(grep -E "#define USB_CAMERA_VERSION_(MAJOR|MINOR|PATCH)" $@/examples/usb_camera_lcd_display/main/main.h | awk '{print $3}')
     version="${version//[$'\t\r\n ']/}"
 
     version_with_dots=$(echo "$version" | sed 's/\(.\)/\1_/g' | sed 's/\_$//')
 
     newfile=$(echo "$file" | sed "s/_sdkconfig.ci.box-3/-ESP-BOX-3-$version_with_dots/g")
-
-    mv "$file" "$newfile"
-
-  elif [[ $file == *usb_camera_sdkconfig.ci.box-lite* ]]; then
-    version=$(grep -E "#define USB_CAMERA_VERSION_(MAJOR|MINOR|PATCH)" $@/examples/usb_camera_lcd_display/main/main.h | awk '{print $3}')
-    version="${version//[$'\t\r\n ']/}"
-
-    version_with_dots=$(echo "$version" | sed 's/\(.\)/\1_/g' | sed 's/\_$//')
-
     newfile=$(echo "$file" | sed "s/_sdkconfig.ci.box-lite/-ESP-BOX-Lite-$version_with_dots/g")
-
-    mv "$file" "$newfile"
-
-  elif [[ $file == *usb_camera_sdkconfig.ci.box* ]]; then
-    version=$(grep -E "#define USB_CAMERA_VERSION_(MAJOR|MINOR|PATCH)" $@/examples/usb_camera_lcd_display/main/main.h | awk '{print $3}')
-    version="${version//[$'\t\r\n ']/}"
-
-    version_with_dots=$(echo "$version" | sed 's/\(.\)/\1_/g' | sed 's/\_$//')
-
     newfile=$(echo "$file" | sed "s/_sdkconfig.ci.box/-ESP-BOX-$version_with_dots/g")
 
     mv "$file" "$newfile"
-  elif [[ $file == *matter_switch_sdkconfig.ci.box-3* ]]; then
+  elif [[ $file == *matter_switch_sdkconfig.ci* ]]; then
     version=$(grep -E "#define MATTER_SWITCH_VERSION_(MAJOR|MINOR|PATCH)" $@/examples/matter_switch/main/app_main.h | awk '{print $3}')
     version="${version//[$'\t\r\n ']/}"
 
     version_with_dots=$(echo "$version" | sed 's/\(.\)/\1_/g' | sed 's/\_$//')
 
     newfile=$(echo "$file" | sed "s/_sdkconfig.ci.box-3/-ESP-BOX-3-$version_with_dots/g")
-
-    mv "$file" "$newfile"
-
-  elif [[ $file == *matter_switch_sdkconfig.ci.box-lite* ]]; then
-    version=$(grep -E "#define MATTER_SWITCH_VERSION_(MAJOR|MINOR|PATCH)" $@/examples/matter_switch/main/app_main.h | awk '{print $3}')
-    version="${version//[$'\t\r\n ']/}"
-
-    version_with_dots=$(echo "$version" | sed 's/\(.\)/\1_/g' | sed 's/\_$//')
-
     newfile=$(echo "$file" | sed "s/_sdkconfig.ci.box-lite/-ESP-BOX-Lite-$version_with_dots/g")
-
-    mv "$file" "$newfile"
-
-  elif [[ $file == *matter_switch_sdkconfig.ci.box* ]]; then
-    version=$(grep -E "#define MATTER_SWITCH_VERSION_(MAJOR|MINOR|PATCH)" $@/examples/matter_switch/main/app_main.h | awk '{print $3}')
-    version="${version//[$'\t\r\n ']/}"
-
-    version_with_dots=$(echo "$version" | sed 's/\(.\)/\1_/g' | sed 's/\_$//')
-
     newfile=$(echo "$file" | sed "s/_sdkconfig.ci.box/-ESP-BOX-$version_with_dots/g")
 
     mv "$file" "$newfile"
